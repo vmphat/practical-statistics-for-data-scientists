4add5ba6fb2ecdf35124da68de95400dc7f44e44
vmphat <vmphat.24@gmail.com>
2024-10-09 04:59:44 +0700

    !clone repo

--- python/notebooks/common.py
+++ python/notebooks/common.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def dataDirectory(dataDirectoryName='data'):
     """
     Return the directory that contains the data.
-    
+
     We assume that the data folder is locate in a parent directory of this file and named 'data'.
     If your setup is different, you will need to change this method.
     """
@@ -13,8 +13,6 @@ def dataDirectory(dataDirectoryName='data'):
         dataDir = dataDir.parent
     found = [d for d in dataDir.rglob('data') if d.is_dir()]
     if not found:
-        raise Exception(f'Cannot find data directory with name {dataDirectoryName} along the path of your source files')
+        raise Exception(
+            f'Cannot find data directory with name {dataDirectoryName} along the path of your source files')
     return found[0]
-    
-    
-    
